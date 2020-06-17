6f7a375c7a7eef83a29158130a00604b518e4170
welearnedtohack <b7ckXwZR6YBcB5Q>
2020-06-17 21:00:56 +0100

    updated

--- main.c
+++ main.c
@@ -25,8 +25,8 @@ typedef struct pagamento{
 typedef struct _lista_pagamentos* lista_pagamentos;
 
 struct _lista_pagamentos{
-    socio *info_pagamento;
-    lista_socios next;
+    pagamento *info_pagamento;
+    lista_pagamentos next;
 };
 
 int id = 0;
@@ -34,6 +34,12 @@ int id = 0;
 void print_socios(lista_socios ls){
     for(;ls != NULL; ls=ls->next){
         printf("%s\t%s\t%d\n", ls->info_socio->nome, ls->info_socio->apelido, ls->info_socio->id);
+    }
+}
+
+void print_pagamentos(lista_pagamentos lp){
+    for(;lp != NULL; lp=lp->next){
+        printf("%d\t%d\n", lp->info_pagamento->montante, lp->info_pagamento->id);
     }
 }
 
@@ -51,29 +57,18 @@ lista_socios inserir_lista_socios(lista_socios lista, socio* s){
     }
 }
 
-lista_socios remove_lista_socios(lista_socios lista, int id_to_remove){
-    lista_socios inicio = lista;
-    lista_socios seguinte;
-    //remover primeiro no
-    if(lista != NULL && lista->info_socio->id == id_to_remove){
-        seguinte = lista->next;
-        free(lista->info_socio);
-        free(lista);
-        return seguinte;
-    }
-    //remover outro no
-    while(lista->next != NULL && lista->next->info_socio->id != id_to_remove){
-        lista = lista->next;
-    }
-    if(lista->next == NULL){
-        fprintf(stderr, "Id nao encontrado\n");
+lista_pagamentos inserir_lista_paga(lista_pagamentos lista, pagamento* p){
+    lista_pagamentos inicio = lista;
+    lista_pagamentos novo_no = calloc(sizeof(struct _lista_pagamentos), 1);
+    novo_no->info_pagamento = p;
+    if(lista == NULL){
+        return novo_no;
     }else{
-        seguinte = lista->next->next;
-        free(lista->next->info_socio);
-        free(lista->next);
-        lista->next = seguinte;
+        while(lista->next != NULL)
+            lista = lista->next;
+        lista->next = novo_no;
+        return inicio;
     }
-    return inicio;
 }
 
 lista_socios menu_add_socio(lista_socios ls){
@@ -112,22 +107,30 @@ lista_socios menu_remove_socio(lista_socios ls) {
 }
 
 void menu_add_paga(){
-    /*pagemento* s = calloc(sizeof(montante),1); // isto não faz sentido?
+    pagamento* p = calloc(sizeof(pagamento),1);
     printf("Montante Pago:\n");
-    scanf("%s", s->montante);
-    s->id = id++;
-    inserir_lista_pagamentos(ls, s);*/
+    scanf("%d", p->montante);
+    p->id = id++;
+    inserir_lista_paga(lp, p);
 }
 
-void menu_edit_paga(){
+void menu_edit_paga(lista_pagamentos lp){
+    lista_pagamentos edit;
+    int id;
+    char string[STRSIZE];
+    printf("ID de sócio: ");
+    scanf("%d^[\n]", &id);
+    for (edit = lp; edit != NULL && edit->info_pagamento->id != id;edit = edit->next);
+    printf("\nMontante: %s\n", edit->info_pagamento->montante);
+    printf("Novo montante: ");
+    scanf("%s^[\n]", string);
+    strcpy(edit->info_pagamento->montante, string);
 
-}
+/* void menu_remove_paga() {
 
-void menu_remove_paga(){
+}*/
 
-}
-
-void write_to_file(lista_socios lista){
+void write_to_file_socios(lista_socios lista){
     FILE *f = fopen("ficheiro_in_socios", "wb");
     while(lista != NULL){
         fwrite(lista->info_socio, 1, sizeof(socio), f);
@@ -136,7 +139,7 @@ void write_to_file(lista_socios lista){
     fclose(f);
 }
 
-lista_socios read_from_file(){
+lista_socios read_from_file_socios() {
     lista_socios lista = NULL;
     FILE *f = fopen("ficheiro_in_socios", "rb");
     socio s;
@@ -152,6 +155,7 @@ lista_socios read_from_file(){
 int main(void) {
     int opcao = 0;
     lista_socios ls = NULL;
+    lista_pagamentos lp = NULL;
 
     while (opcao != 4){
         printf("1 - adicionar socio\n");
@@ -161,8 +165,9 @@ int main(void) {
         printf("5 - editar pagamento\n");
         printf("6 - remover pagamento\n");
         printf("7 - imprimir socio\n");
-        printf("8 - write socios to file\n");
-        printf("9 - read socios from file\n");
+        printf("8 - imprimir pagamentos")
+        printf("9 - write socios to file\n");
+        printf("10 - read socios from file\n");
         scanf("%d", &opcao);
         switch (opcao) {
             case 1:
@@ -175,7 +180,7 @@ int main(void) {
                 ls = menu_remove_socio(ls);
                 break;
             case 4:
-                menu_add_paga();
+                lp = menu_add_paga(lp);
                 break;
             case 5:
                 menu_edit_paga();
@@ -187,14 +192,16 @@ int main(void) {
                 print_socios(ls);
                 break;
             case 8:
-                write_to_file(ls);
+                print_pagamentos(lp);
                 break;
             case 9:
-                ls = read_from_file();
+                write_to_file_socios(ls);
+                break;
+            case 10:
+                ls = read_from_file_socios();
                 break;
             default:
                 break;
         }
     }
-
 }
